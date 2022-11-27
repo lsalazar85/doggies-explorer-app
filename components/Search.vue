6d46b049ee571f36d4e1110254dc5c2edd3dbf48
Luis Salazar <l_salazar85@icloud.com>
2022-11-27 20:24:39 -0300

    wip(search): add input value for metadata request

--- components/Search.vue
+++ components/Search.vue
@@ -1,22 +1,31 @@
+<script lang="ts" setup>
+import Web3 from 'web3';
+import { ref } from 'vue';
+import ABI from '../constants/ABI.json';
+
+const inputTokenId = ref('');
+
+const onSubmit = async () => {
+  try {
+    const web3 = new Web3('https://mainnet.infura.io/v3/c83c37fa3b754594ada81272a37fa9b7');
+    const address = '0xC7dF86762ba83f2a6197e1Ff9Bb40ae0f696B9E6';
+    const contract = new web3.eth.Contract(ABI, address);
+    const nftMetadata = await contract.methods.tokenURI(inputTokenId.value).call().then((resolve:string) => resolve);
+
+    const response = await fetch(`${nftMetadata}`);
+    const metadata = await response.json();
+    console.log(metadata)
+  } catch(e){
+    console.log(e)
+  }
+}
+</script>
+
 <template>
-  <div class="searchContainer">
-    <span>Search</span>
-    <form>
-      <input type="text" placeholder="search" />
+  <div class="search-container">
+    <form @submit.prevent="onSubmit()">
+      <input v-model.number="inputTokenId" placeholder="search" />
       <button type="submit">button</button>
     </form>
   </div>
 </template>
-
-<script lang="ts">
- /* const Web3 = require('web3');
-  const abi = require('../constants/ABI.json');
-
-  const web3 = new Web3('https://mainnet.infura.io/v3/c83c37fa3b754594ada81272a37fa9b7');
-  const address = '0xC7dF86762ba83f2a6197e1Ff9Bb40ae0f696B9E6';
-  const tokenId = 7635;
-
-  const contract = new web3.eth.Contract(abi, address);
-  const nftMetadata = contract.methods.tokenURI(tokenId).call().then((resolve:string) => console.log(resolve));
-  console.log(nftMetadata); */
-</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -46,9 +46,7 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    analyze: true,
-  },
+  build: {},
 
   styleResources: {
     scss: [
